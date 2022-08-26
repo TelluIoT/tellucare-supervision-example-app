74d4995495a97ad4d09faef8252f8461996fad51
Richard Frostmark <richard.frostmark@gmail.com>
2022-08-26 12:18:42 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# tellucare-supervision-example-app
-Demo app to show a video stream
+# Getting started with Embedded video player
+
+TelluCare Supervision supports playing videostreams for a patient in embedded video views for your web page or mobile apps. This is an example on how to implement TelluCare supvervision camera views in your Xamarin applications.
+
+# Adding embedded camera view to your application
+
+The Tellucare supervision embedded player can be included in your app by using a webview.
+
+    <WebView Source="https://<ENVIRONMENT>/viewPatient/<PATIENT_ID>[<QUERRY_PARAMS>][#token=<AUTHENTICATION_TOKEN>]"/>
+	
+Format for the url in Source, see [Getting started with Embedded video player](https://calm-sky-0f8781003-1.westeurope.azurestaticapps.net/guide/embedded/embeddedVideoPlayer.html)
---
+++ tellucare-supervision-example-app/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace tellucare_supervision_example_app
+{
+    public partial class MainPage : ContentPage
+    {
+        public string urlToShow = "";
+        public string UrlToShow
+        {
+            get { return urlToShow; }
+            set
+            {
+                urlToShow = value;
+                OnPropertyChanged(nameof(UrlToShow)); // Notify that there was a change on this property
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            url.Text = "https://tellucare-embedded-dev.tellucloud.com/tilsyn-dev/viewPatient/SrT6vvyNHFFmZ0UiSWcb0q";
+        }
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            string urlStr = url.Text + "?autoPlay=true&hideControls=true";
+            if(!String.IsNullOrEmpty(color.Text))
+            {
+                urlStr += "&color=" + color.Text;
+            }
+            if (!String.IsNullOrEmpty(token.Text))
+            {
+                urlStr += "#token=" + token.Text;
+            }
+            UrlToShow = urlStr;
+            // videoComp.Source = "https://tellucare-embedded-dev.tellucloud.com/tilsyn-dev/viewPatient/SrT6vvyNHFFmZ0UiSWcb0q";
+            videoComp.Source = urlStr;
+            // await label.RelRotateTo(360, 1000);
+        }
+    }
+}
