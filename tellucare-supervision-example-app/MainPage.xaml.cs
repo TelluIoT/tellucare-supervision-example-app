81dcd44043b647a13ff8fc0e7b519e1daae8a7fb
Richard Frostmark <richard.frostmark@gmail.com>
2022-09-08 10:56:37 +0200

    Add health provider and patient id

--- tellucare-supervision-example-app/MainPage.xaml.cs
+++ tellucare-supervision-example-app/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace tellucare_supervision_example_app
@@ -11,6 +12,7 @@ namespace tellucare_supervision_example_app
     public partial class MainPage : ContentPage
     {
         public string urlToShow = "";
+        public bool showUrl = false;
         public string UrlToShow
         {
             get { return urlToShow; }
@@ -20,24 +22,49 @@ namespace tellucare_supervision_example_app
                 OnPropertyChanged(nameof(UrlToShow));
             }
         }
+        public bool ShowUrl
+        {
+            get { return showUrl; }
+            set
+            {
+                showUrl = value;
+                OnPropertyChanged(nameof(ShowUrl));
+            }
+        }
         public MainPage()
         {
             InitializeComponent();
             BindingContext = this;
-            url.Text = "";
+            healthProvider.Text = "";
         }
         void OnButtonClicked(object sender, EventArgs args)
         {
-            string urlStr = url.Text + "?autoPlay=true&hideControls=true";
-            if(!String.IsNullOrEmpty(color.Text))
+            bool firstParam = true;
+            Func<string, string, string> addDivider = (tag, value) => {
+                if (String.IsNullOrEmpty(tag) || String.IsNullOrEmpty(value))
+                {
+                    return "";
+                }
+                string res = (firstParam ? "#" : "/") + tag + "=" + value;
+                firstParam = false;
+                return res;
+            };
+            if (String.IsNullOrEmpty(healthProvider.Text) || String.IsNullOrEmpty(patientId.Text))
             {
-                urlStr += "&color=" + color.Text;
+                return;
             }
-            if (!String.IsNullOrEmpty(token.Text))
-            {
-                urlStr += "#token=" + token.Text;
-            }
+            string urlStr = String.Format("https://tellucare-embedded-dev.tellucloud.com/{0}/viewPatient/{1}", healthProvider.Text, patientId.Text);
+            // add token
+            urlStr += addDivider("token", token.Text);
+            // add color
+            urlStr += addDivider("color", color.Text);
+            // add autoPlay
+            urlStr += addDivider("autoPlay", "true");
+            // add hideControls
+            urlStr += addDivider("hideControls", "true");
+            // update label with url
             UrlToShow = urlStr;
+            // update source attribute on webview component
             videoComp.Source = urlStr;
         }
     }
